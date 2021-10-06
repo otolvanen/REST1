2a884ea41ed1d1605a63880b063a4fc92be3881a
otolvanen <67708565+otolvanen@users.noreply.github.com>
2021-10-06 18:21:28 +0300

    -ui

--- app.js
+++ app.js
@@ -3,14 +3,78 @@ const express = require("express");
 const fs = require("fs");
 const body_parser = require("body-parser");
 const app = express();
+const cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Terska");
+/*CORS isn’t enabled on the server, this is due to security reasons by default,
+so no one else but the webserver itself can make requests to the server.*/
+// Add headers
+app.use(cors());
+
+
+
+
+
+
+
+app.get("/get", (req, res, next) => {
+  let obj = fs.readFileSync('text_json.txt',  {encoding:'utf8', flag:'r'});
+  var r = req.query.form;
+  console.log(req.query);
+  r = JSON.parse(r);
+  
+  r = r[0].value;
+  obj = JSON.parse(obj);
+  var sana = "Ei tietoo :(";
+  for(var i of obj){
+    if (r == i.suomeksi){
+      sana = i.englanniksi;
+    }
+  }
+  console.log("lähetetään", sana);
+  var j = {"value": sana}
+  res.send(j);
 });
 
+app.post("/post", (req, res, next) => {
+  const r = req.query.form;
+  console.log(r, "postia");
+  r = JSON.parse(r);
+  var f_val = r[0].value;
+  var e_val = r[1].value;
+  let obj = fs.readFileSync('text_json.txt',  {encoding:'utf8', flag:'r'});
+  obj = JSON.parse(obj);
+  var onko = false;
+  for(var i of obj){
+    if (f_val == i.suomeksi){
+      onko = true;
+    }
+  }
+  if (onko == false){
+    console.log(JSON.stringify(r.fin), "lol");
+
+    obj.push({"suomeksi":f_val, "englanniksi" : e_val});
+    obj = JSON.stringify(obj);
+    console.log(obj, "str")
+    fs.writeFileSync('text_json.txt', obj);
+    var k = {"value": ["lisatty", e_val]}
+    res.send(k);
+  }else{
+    var ei = {"value": "on jo sanakirjassa"}
+    res.send(ei);
+  }
+
+});
+
+app.listen(3000, () => {
+  console.log("kuntelen");
+});
+
+
+
+/*
 function tee_map() {
   var obj = fs.readFileSync("./text.txt").toString();
 
@@ -60,7 +124,5 @@ app.post("/post/post", (req, res) => {
   }
   res.send(vast);
 });
+*/
 
-app.listen(3000, () => {
-  console.log("kuntelen");
-});
